3601c6e58bc3db387af733e64b98a7a0bf8f32aa
Yegorisa <yegor1998@gmail.com>
2017-12-04 13:55:27 +1300

    Added primary design

--- app/src/main/java/com/egoregorov/colourmemory/MainActivityFragment.java
+++ app/src/main/java/com/egoregorov/colourmemory/MainActivityFragment.java
@@ -1,15 +1,22 @@
 package com.egoregorov.colourmemory;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private static final String TAG = "MainActivityFragment";
+
+    View mView;
+    GridView mBoard;
 
     public MainActivityFragment() {
     }
@@ -17,6 +24,13 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        mView = inflater.inflate(R.layout.fragment_main, container, false);
+        mBoard = mView.findViewById(R.id.board);
+        mBoard.setAdapter(new BoardAdapter(getActivity(),mView));
+        mBoard.setOnItemClickListener((parent, v, position, id) -> Toast.makeText(getContext(), "" + position,
+                Toast.LENGTH_SHORT).show());
+        Log.d(TAG, "onCreateView: SIZE IS "+ mView.getHeight());
+        return mView;
     }
+
 }
